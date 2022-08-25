4a2cb9e565112551b1e1a5fb6caa5e30bf9348a7
bobby1823 <sayanta.roy@lowes.com>
2022-08-25 23:35:12 +0530

    Array: Container_With_Most_Water.java

---
+++ src/io/ghost/datastructure/arraystring/Container_With_Most_Water.java
@@ -0,0 +1,22 @@
+package io.ghost.datastructure.arraystring;
+
+public class Container_With_Most_Water {
+    public static void main(String[] args) {
+        System.out.println("Water amount: "+ maxArea(new int[]{1,2,1}));
+    }
+
+    public static int maxArea(int[] height) {
+        int max = 0;
+        int leftPointer = 0, rightPointer = height.length-1;
+        while (leftPointer < rightPointer) {
+            if (height[leftPointer] < height[rightPointer]) {
+                max = Integer.max(max, height[leftPointer]* (rightPointer-leftPointer));
+                leftPointer += 1;
+            } else {
+                max = Integer.max(max, height[rightPointer]* (rightPointer-leftPointer));
+                rightPointer -= 1;
+            }
+        }
+        return max;
+    }
+}
